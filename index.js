21b308ea038dfe52f7690f3efdc56359831a5e38
Haseeb MernStack <seebpubg@gmail.com>
2025-04-23 20:48:12 +0500

    Updated

--- index.js
+++ index.js
@@ -39,17 +39,52 @@ let setTargetColor = () => {
     document.getElementById("target-color").textContent = targetColor;
 }
 
-let initializeGame = () => { }
+let formateTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`
+}
 
 let updateTimer = () => {
     timer--;
     document.getElementById('timer').textContent = formateTime(timer);
 }
 
-let formateTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`
+let handleClick = (e) => {
+    const clickedColor = e.target.getAttribute("data-color");
+    if (clickedColor === targetColor) {
+        score++;
+        document.getElementById("score").textContent = score;
+
+        if (score === winingScore) {
+            endGame(true);
+        }
+        setRandomColor();
+        setTargetColor();
+        document.getElementById("correct-music").play();
+    } else {
+        document.getElementById("incorrect-music").play();
+    }
 }
+
+let initializeGame = () => {
+    score = 0;
+    timer = 120;
+    document.getElementById("score").textContent = score;
+    document.getElementById("timer").textContent = formateTime(timer);
+    document.getElementById("congrats-overlay").style.display = "none";
+    document.getElementById("lose-overlay").style.display = "none";
+    setRandomColor();
+    setTargetColor();
+
+    const bgm = document.getElementById("background-music");
+    // bgm.play();
+
+    gameInterval = setInterval(setRandomColor, 1000);
+    timerInterval = setInterval(updateTimer, 1000);
+}
+
+initializeGame();
+
 
 let endGame = () => { }
